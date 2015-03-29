f33940ac62ea73900f72533a31c9e1544e44d334
brydenfogelman <bryden1995@gmail.com>
2015-03-28 17:55:58 -0700

    put html on server, added .js file directly into html
    Was getting a lot of cross-browser issues (some security thing, can’t
    talk between local files and localhost)
    
    Had to download ejs in order to render the html to the page (requires
    more investigation)
    
    Gonna spend a little bit of time spicing it up and cleaning up the code

--- test-app/simple-app.js
+++ test-app/simple-app.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
 })
 */
 
+//var __dirname = '/Users/brydenfogelman/Desktop/ghthouse/playing-with-express/test-app';
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/home', function (req, res) {
+    res.render('index.html');
+    //res.send('hello');
+})
+
 app.get('/people', function (req, res) {
     var json_data = [{
        name: 'john smith',
@@ -24,8 +36,8 @@ app.get('/people', function (req, res) {
     //res.send("hello world")
 })
 
-app.listen(3000);
-/*
+//app.listen(3000);
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address
@@ -34,4 +46,3 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
-*/
