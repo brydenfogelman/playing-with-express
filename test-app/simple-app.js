581120d7c0a483904bf0f7d80505f2bbaf0610b6
brydenfogelman <bryden1995@gmail.com>
2015-04-08 18:02:43 -0700

    small changes

--- test-app/simple-app.js
+++ test-app/simple-app.js
@@ -1,25 +1,29 @@
+/*
+Simple exercise to learn Ajax and Servers!
+(specifically express)
+by bryden fogelman
+*/
+
 var express = require('express')
 var app = express()
 
-
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-*/
-
-//var __dirname = '/Users/brydenfogelman/Desktop/ghthouse/playing-with-express/test-app';
-
+// tell the app to use the static express
 app.use(express.static(__dirname + '/public'));
+// set views to its directory
 app.set('views', __dirname + '/public/views');
+// use ejs to render the html file to the page, 
 app.engine('html', require('ejs').renderFile);
+// view engine to render to html
 app.set('view engine', 'html');
 
-app.get('/home', function (req, res) {
+// when the local host is loaded, it will load index
+app.get('/', function (req, res) {
     res.render('index.html');
     //res.send('hello');
 })
 
+// have some hardcoded test to send once theres an ajax request
+// at /people
 app.get('/people', function (req, res) {
     var json_data = [{
        name: 'john smith',
@@ -31,13 +35,16 @@ app.get('/people', function (req, res) {
        age: 54,
        citizenship: 'USA'
     }];
-    //console.log('something: ' + JSON.stringify
+    
+    //sends the json response
     res.json(json_data);
     //res.send("hello world")
 })
 
+// I think the two below are interchangeable
+// (with some minor differences)
+// this requires some more investigation!
 //app.listen(3000);
-
 var server = app.listen(3000, function () {
 
   var host = server.address().address
